b3df31eda2a7cd557148f88a5da9887fa5ec88ef
OuafidHabchi <habchi.ouafid@gmail.com>
2024-10-09 16:35:14 -0400

    Update Bot_test-1.py

--- Bot_test-1.py
+++ Bot_test-1.py
@@ -99,7 +99,11 @@ if submit_button and user_message:
             else:
                 st.session_state["messages"].append({"sender": "bot", "message": "Je n'ai pas compris votre question."})
     else:
-        st.session_state["messages"].append({"sender": "bot", "message": "Je n'ai pas compris votre question. Pouvez-vous la répéter SVP ?"})
+       st.session_state["messages"].append({
+    "sender": "bot",
+    "message": "Je n'ai pas compris votre question. Pouvez-vous la répéter SVP ? / I did not understand your question. Could you please repeat it?"
+})
+
 
     # Forcer le conteneur de chat à scroller jusqu'au bas après l'ajout des messages
     with chat_container.container():
